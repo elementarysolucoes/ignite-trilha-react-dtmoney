6e4e497d7615aac0be5c290b80dd4012d850e021
Jean Lobo <jeanloboxd@gmail.com>
2021-07-06 23:28:58 -0300

    criado save data form

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
+import schema from 'miragejs/orm/schema';
 
 createServer({
+  
+  models: {
+    transition: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer',
+          type:'deposit',
+          category: 'Development',
+          amount: 6000,
+          createdAt: new Date('2021-01-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type:'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-01-14 11:00:00')
+        }
+      ]
+    })
+  },
+  
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Desenvolvimento de website',
-          amount: 12.000,
-          type: 'deposit' ,
-          category: 'Venda',
-          createAt: new Date()
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      
+      const data = JSON.parse(request.requestBody);
+      return schema.create('transaction', data);
+
     })
   }
 
